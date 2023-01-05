c4d57daa961cf98a6d126404752e8bfc6751f77f
Zhigaras Ilya <jigaras@gmail.com>
2023-01-06 00:08:28 +0500

    initial commit

---
+++ recyclerview_advanced/app/src/main/java/com/zhigaras/rickandmortypagination/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.zhigaras.rickandmortypagination
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.zhigaras.rickandmortypagination.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+    
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MainViewModel by viewModels()
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        
+        
+    }
+    
+}
---
+++ recyclerview_advanced/app/src/main/java/com/zhigaras/rickandmortypagination/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.zhigaras.rickandmortypagination
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
